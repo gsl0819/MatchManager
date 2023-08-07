41e70607b38dceab7da53005bf94c87fa8ff822d
mumu <gslgsl2000@163.com>
2023-08-08 00:57:05 +0800

    v1.1.2

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
